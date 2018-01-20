2b0302a855caa6e5fa8d6bd7cc5e31c266dc0412
pj <pj@pj.com>
2018-01-20 13:45:02 -0500

    add form for messages and show incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,34 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function(chat) {
     console.log('New Message', chat);
+    const li = jQuery('<li></li>');
+    li.text(`${chat.from}: ${chat.text}`);
+
+    jQuery('#messages').append(li);
 });
+
+// socket.emit('createMessage', {
+//     from: 'Shaalu',
+//     text: 'Poda'
+// }, function(data) {
+//     console.log('Got it!', data);
+// });
+
+// $(function() {
+    
+//     $('#message-form').submit(function(e) {
+//     console.log('Received');
+//     e.preventDefault();
+// });
+
+// });
+
+jQuery('#message-form').on('submit',function(e){
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function() {
+
+    });
+  });
